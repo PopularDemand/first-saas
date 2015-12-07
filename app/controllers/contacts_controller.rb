9301dc604e3b218a806ef1d2c309a9ea047f4184
AnderAl <alexa.anderson@outlook.com>
2015-12-07 16:51:31 -0600

    added contact mailer and view and controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,6 @@
 class ContactsController < ApplicationController
 	def new
-		#used in new.html.erb view
+		#used in new.html.erb view, needed to generate form
 		@contact = Contact.new
 	end
 
@@ -9,6 +9,12 @@ class ContactsController < ApplicationController
 
 		#just putting contact.save saves the code to database
 		if @contact.save
+			name = params[:contact][:name]
+			email = params[:contact][:email]
+			body = params[:contact][:comments]
+
+			ContactMailer.contact_email(name, email, body).deliver
+			
 			flash[:success] = 'Message Sent.'
 			redirect_to new_contact_path
 		else
