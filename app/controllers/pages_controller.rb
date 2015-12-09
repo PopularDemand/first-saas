ae28ba09031ca2931cd41c6127c3680ed95267f1
AnderAl <alexa.anderson@outlook.com>
2015-12-09 16:51:41 -0600

    modified sign up forms

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 	def home
 		#used in the home.html to show which plan is selected to register for
-		@basic_play = Plan.find(1)
+		@basic_plan = Plan.find(1)
 		@pro_plan = Plan.find(2)
 	end
 
