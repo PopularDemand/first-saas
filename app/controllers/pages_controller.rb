5fea358b63d6bbdd61eee0b1c04fba8de2e46d1e
AnderAl <alexa.anderson@outlook.com>
2015-12-09 16:05:55 -0600

    added plan vaiables and params to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
 	def home
+		#used in the home.html to show which plan is selected to register for
+		@basic_play = Plan.find(1)
+		@pro_plan = Plan.find(2)
 	end
 
 	def about
