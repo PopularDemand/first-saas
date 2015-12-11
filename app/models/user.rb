d8dc234a5ddd6c2bdecb7b795602298a062ae8e8
AnderAl <alexa.anderson@outlook.com>
2015-12-10 20:55:35 -0600

    added notice and styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   	if valid?
   		#sends info to stripe
   	  customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
-  	  self.stripe_customer_token = customer.plan_id
+  	  self.stripe_customer_token = customer.id
   	  save!
   	end
   end
