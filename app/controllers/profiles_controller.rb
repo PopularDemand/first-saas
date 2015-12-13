d44979431a3cdc8013a9dc65a7f03ea2926fafcc
AnderAl <alexa.anderson@outlook.com>
2015-12-13 00:06:47 -0600

    added profiles new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,13 @@
 class ProfilesController < ApplicationController
+	# controllers have access to query string parameters
+	def new
+		# Form for each user to create their page
+		# Gets active users id
+		@user = User.find(params[:user_id])
+		# build_profile available because user model nested with profile
+		@profile = @user.build_profile
+	end
+
 	def create
 	end
 end
