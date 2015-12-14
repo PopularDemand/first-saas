2218347da2c49f17831b671cdcc106f2d93d19b6
AnderAl <alexa.anderson@outlook.com>
2015-12-13 18:36:31 -0600

    added before filters to check correct user for profile and users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_action :authenticate_user!
+	
 	def show
 		#use id instead of user_id based on the url format (just has :id, not :user_id)
 		@user = User.find( params[:id] )
