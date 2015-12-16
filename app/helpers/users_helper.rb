c9b3c2ee5a5f514b47cabd05e6b03e18023c13b7
AnderAl <alexa.anderson@outlook.com>
2015-12-16 15:24:43 -0600

    made improvements to user profile page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+
+	def job_title_icon
+		# instance variable is defined in controller
+		if @user.profile.job_title == "Developer"
+			"<i class='fa fa-code'></i>".html_safe
+		elsif @user.profile.job_title == "Entrepreneur"
+			"<i class='fa fa-lightbulb-o'></i>".html_safe
+		elsif
+			"<i class='fa fa-dollar'></i>".html_safe
+		end
+	end
+	
 end
